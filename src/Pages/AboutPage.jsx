7d2b8e42ca129cb9365faf3deba895c00039c961
Rake Wangsa <rakekw28@gmail.com>
2024-07-16 07:08:45 +0700

    update about us

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,9 +1,13 @@
 import Footer from '../Components/Footer/Footer';
 import About from '../Components/Home/About';
 import Navbar from '../Components/Navbar/Navbar';
-import gambar from '../assets/images/rake.png';
+import gambar from '../assets/images/rakev2.jpeg';
 import gambar2 from '../assets/images/ito.jpeg';
 import gambar3 from '../assets/images/idan.png';
+import gambar4 from '../assets/images/suares.jpeg';
+import gambar5 from '../assets/images/emil.jpeg';
+import gambar6 from '../assets/images/bagasreswara.png';
+
 import { FaInstagram, FaWhatsapp, FaGithub } from 'react-icons/fa';
 import { useEffect } from 'react';
 import Chat from '../Components/Chat/Chat';
@@ -30,9 +34,9 @@ const AboutPage = () => {
           <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-3 sm:gap-16 px-12 sm:px-16">
             <div className="relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-lg rounded-lg group">
               <div className=" front-face text-center">
-                <img className="w-36 h-36 rounded-full mb-4" src={gambar} alt="" />
+                <img className="w-48 h-48 rounded-full mb-4" src={gambar} alt="" />
                 <h4 className="font-bold">Rake Wangsa</h4>
-                <p>Full-Stack Developer</p>
+                <p>Tech Lead</p>
               </div>
               {/* <div className="absolute top-0 left-0 w-full h-full p-8 text-center back-face transform rotate-y-180 ">
                 <h2 className="font-bold mb-5">About Me</h2>
@@ -52,9 +56,9 @@ const AboutPage = () => {
             </div>
             <div className="relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-md rounded-lg group">
               <div className=" front-face text-center">
-                <img className="w-36 h-36 rounded-full mb-4 object-cover" src={gambar2} alt="" />
+                <img className="w-48 h-48 rounded-full mb-4 object-cover" src={gambar2} alt="" />
                 <h4 className="font-bold">Ito Distiano</h4>
-                <p>Front-End Developer</p>
+                <p>Full-Stack Developer</p>
               </div>
               {/* <div className="absolute top-0 left-0 w-full h-full p-8 text-center back-face transform rotate-y-180">
                 <h2 className="font-bold mb-5">About Me</h2>
@@ -74,7 +78,7 @@ const AboutPage = () => {
             </div>
             <div className=" relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-md rounded-lg group">
               <div className=" front-face text-center ">
-                <img className="w-36 h-36 rounded-full mb-4 object-cover" src={gambar3} alt="" />
+                <img className="w-48 h-48 rounded-full mb-4 object-cover" src={gambar3} alt="" />
                 <h4 className="font-bold">Wildan Mirtaza</h4>
                 <p>Back-End Developer</p>
               </div>
@@ -94,6 +98,76 @@ const AboutPage = () => {
                 </div>
               </div> */}
             </div>
+            
+            <div className="relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-lg rounded-lg group">
+              <div className=" front-face text-center">
+                <img className="w-48 h-48 rounded-full mb-4" src={gambar4} alt="" />
+                <h4 className="font-bold">Naufal Wafi</h4>
+                <p>Back-end Developer</p>
+              </div>
+              {/* <div className="absolute top-0 left-0 w-full h-full p-8 text-center back-face transform rotate-y-180 ">
+                <h2 className="font-bold mb-5">About Me</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, commodi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi iste natus explicabo aperiam, fugiat libero.</p>
+                <div className="flex justify-center mt-5">
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaWhatsapp className="size-6" style={{ color: '#25D366' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaInstagram className="size-6" style={{ color: '#c32aa3' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaGithub className="size-6" />
+                  </div>
+                </div>
+              </div> */}
+            </div>
+
+            <div className="relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-lg rounded-lg group">
+              <div className=" front-face text-center">
+                <img className="w-48 h-48 rounded-full mb-4" src={gambar5} alt="" />
+                <h4 className="font-bold">Emil Al Fikri</h4>
+                <p>UI/UX Designer</p>
+              </div>
+              {/* <div className="absolute top-0 left-0 w-full h-full p-8 text-center back-face transform rotate-y-180 ">
+                <h2 className="font-bold mb-5">About Me</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, commodi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi iste natus explicabo aperiam, fugiat libero.</p>
+                <div className="flex justify-center mt-5">
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaWhatsapp className="size-6" style={{ color: '#25D366' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaInstagram className="size-6" style={{ color: '#c32aa3' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaGithub className="size-6" />
+                  </div>
+                </div>
+              </div> */}
+            </div>
+
+            <div className="relative h-80 flex flex-col justify-center items-center border border-slate-400 shadow-lg rounded-lg group">
+              <div className=" front-face text-center">
+                <img className="w-48 h-48 rounded-full mb-4" src={gambar6} alt="" />
+                <h4 className="font-bold">Bagas Reswara</h4>
+                <p>Front-End Developer</p>
+              </div>
+              {/* <div className="absolute top-0 left-0 w-full h-full p-8 text-center back-face transform rotate-y-180 ">
+                <h2 className="font-bold mb-5">About Me</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, commodi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi iste natus explicabo aperiam, fugiat libero.</p>
+                <div className="flex justify-center mt-5">
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaWhatsapp className="size-6" style={{ color: '#25D366' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaInstagram className="size-6" style={{ color: '#c32aa3' }} />
+                  </div>
+                  <div className="bg-slate-200 p-1 rounded-lg mx-2">
+                    <FaGithub className="size-6" />
+                  </div>
+                </div>
+              </div> */}
+            </div>
+
           </div>
         </div>
       </div>
